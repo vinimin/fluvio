7f5190a2520bbbb7b28ebe5414b14a81514de1a9
Sehyo Chang <sehyo@fluvio.io>
2020-01-09 14:50:06 -0800

    test asyn callback

--- src/client-node/src/lib.rs
+++ src/client-node/src/lib.rs
@@ -24,6 +24,12 @@ use nj_sys::napi_get_cb_info;
 use nj_sys::napi_get_global;
 use nj_sys::napi_call_function;
 use nj_sys::napi_create_function;
+use nj_sys::napi_create_threadsafe_function;
+use nj_sys::napi_call_threadsafe_function;
+use nj_sys::napi_threadsafe_function_call_mode_napi_tsfn_blocking;
+use nj_sys::napi_threadsafe_function;
+use nj_sys::napi_release_threadsafe_function;
+use nj_sys::napi_threadsafe_function_release_mode_napi_tsfn_release;
 
 use flv_future_core::spawn;
 use flv_future_core::sleep;
@@ -60,11 +66,15 @@ pub extern "C" fn hello_callback(env: napi_env,info: napi_callback_info) -> napi
   
     let mut cb: napi_value = args[0];
 
-    let mut argv: [napi_value; 1] = [ptr::null_mut(); 1];
+    let mut label = ptr::null_mut();
     
-    
-    let status = unsafe { napi_create_string_utf8(env,b"hello\x00" as *const u8 as *const c_char, NAPI_AUTO_LENGTH as usize, argv.as_mut_ptr()) };
-    assert_eq!(status,napi_status_napi_ok);
+    assert_eq!(unsafe { 
+        napi_create_string_utf8(
+            env,
+            b"hello\x00" as *const u8 as *const c_char,
+            NAPI_AUTO_LENGTH as usize, 
+            &mut label) 
+        },napi_status_napi_ok);
   
     
     let mut global = ptr::null_mut();
@@ -72,17 +82,155 @@ pub extern "C" fn hello_callback(env: napi_env,info: napi_callback_info) -> napi
     assert_eq!(status,napi_status_napi_ok);
   
     let mut result = ptr::null_mut();
-    let status = unsafe { napi_call_function(env, global, cb, 1, argv.as_mut_ptr(), &mut result) };
+    let status = unsafe { napi_call_function(env, global, cb, 1, &mut label, &mut result) };
     assert_eq!(status, napi_status_napi_ok);
+
+    return ptr::null_mut()
+
+}
+
+
+// convert the rust data into JS
+pub extern "C" fn hello_callback_js(
+    env: napi_env,
+    js_cb: napi_value, 
+    context: *mut ::std::os::raw::c_void,
+    data: *mut ::std::os::raw::c_void) {
+
+    if (env != ptr::null_mut()) {
+
+        let mut label = ptr::null_mut();
+    
+        assert_eq!(
+            unsafe { 
+                napi_create_string_utf8(
+                env,
+                b"hello\x00" as *const u8 as *const c_char,
+                NAPI_AUTO_LENGTH as usize, 
+                &mut label) 
+            },
+            napi_status_napi_ok);
+
+        let mut global = ptr::null_mut();
+        let status = unsafe { napi_get_global(env, &mut global) };
+        assert_eq!(status,napi_status_napi_ok);
+
+        let mut result = ptr::null_mut();
+        assert_eq!(
+            unsafe { 
+                napi_call_function(
+                    env, 
+                    global, 
+                    js_cb, 
+                    1, 
+                    &mut label, 
+                    &mut result) 
+            }, 
+            napi_status_napi_ok);
+    }
+    
+
+}
+
+struct TsFn(napi_threadsafe_function);
+
+unsafe impl Sync for TsFn{}
+unsafe impl Send for TsFn{}
+
+#[no_mangle]
+pub extern "C" fn hello_callback_async(env: napi_env,info: napi_callback_info) -> napi_value {
+  
+    let mut argc: size_t  = 1;
+
+    let mut args: [napi_value; 1] = [ptr::null_mut(); 1];
+
+    let mut tsfn = ptr::null_mut();
+    
+
+    // retrieve callback function
+    assert_eq!(
+        unsafe { 
+            napi_get_cb_info(
+                env, 
+                info, 
+                &mut argc,
+                args.as_mut_ptr(),
+                ptr::null_mut(), 
+                ptr::null_mut())
+        }, 
+        napi_status_napi_ok);
+
+    let mut js_cb: napi_value = args[0];
+
+    let mut work_name = ptr::null_mut();
+
+    assert_eq!(
+        unsafe {
+            napi_create_string_utf8(
+            env,
+            b"async \x00" as *const u8 as *const c_char,
+            NAPI_AUTO_LENGTH as usize,
+            &mut work_name)
+        },
+        napi_status_napi_ok);
+
+    // convert the callback
+    assert_eq!(
+        unsafe { 
+            napi_create_threadsafe_function(
+                env,
+                js_cb,
+                ptr::null_mut(),
+                work_name,
+                0,
+                1,
+                ptr::null_mut(),
+                None,
+                ptr::null_mut(),
+                Some(hello_callback_js),
+                &mut tsfn)
+                },
+        napi_status_napi_ok);
+
+
+    let xtsfn = TsFn(tsfn);
+
+    spawn(async move {
+            
+            println!("sleeping");
+            sleep(Duration::from_secs(3)).await;
+            println!("woke from time");
+
+            let inner_fn = xtsfn.0;
+            
+            assert_eq!(
+                unsafe {
+                    napi_call_threadsafe_function(
+                    inner_fn,
+                    ptr::null_mut(),
+                    napi_threadsafe_function_call_mode_napi_tsfn_blocking)
+                    },
+                napi_status_napi_ok);
+
+            assert_eq!(
+                unsafe {
+                    napi_release_threadsafe_function(
+                        inner_fn,
+                        napi_threadsafe_function_release_mode_napi_tsfn_release)
+                }, 
+                napi_status_napi_ok)
+            
+        
+    });
 
     return ptr::null_mut()
 
   }
 
 
-/*
+
 #[no_mangle]
-pub extern "C" fn init_hello (env: napi_env, exports: napi_value ) -> napi_value{
+pub extern "C" fn init_hello_descriptor (env: napi_env, exports: napi_value ) -> napi_value{
     
     println!("registering hello module");
     
@@ -101,7 +249,7 @@ pub extern "C" fn init_hello (env: napi_env, exports: napi_value ) -> napi_value
     assert_eq!(status, napi_status_napi_ok);
     return exports;
 }
-*/
+
 
 
 #[no_mangle]
@@ -119,6 +267,22 @@ pub extern "C" fn init_hello (env: napi_env, exports: napi_value ) -> napi_value
 
 
 
+#[no_mangle]
+pub extern "C" fn init_async_hello_descriptor (env: napi_env, exports: napi_value ) -> napi_value{
+    
+    println!("registering hello module");
+
+    let mut new_exports = ptr::null_mut();
+    let status = unsafe { napi_create_function(env, b"x00" as *const u8 as *const c_char, NAPI_AUTO_LENGTH as usize,
+         Some(hello_callback_async), ptr::null_mut(), &mut new_exports) };
+  
+    assert_eq!(status,napi_status_napi_ok);
+    return new_exports;
+    
+}
+
+
+
 
 #[ctor]
 fn init_module() {
@@ -131,7 +295,7 @@ fn init_module() {
         nm_version: NAPI_VERSION as i32,
         nm_flags: 0,
         nm_filename: b"test.rs\x00" as *const u8 as *const c_char,
-        nm_register_func: Some(init_hello),
+        nm_register_func: Some(init_async_hello_descriptor),
         nm_modname:  b"hello\x00" as *const u8 as *const c_char,
         nm_priv: ptr::null_mut(),
         reserved: [ptr::null_mut(),ptr::null_mut(),ptr::null_mut(),ptr::null_mut()]
